7ac43f00bf4a6e3ee665e96dd0ee2ea3284d60ed
Naduni-samindi <samindinaduni@gmail.com>
2025-01-21 16:06:30 +0530

    Add my files

---
+++ Naduni/assest/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Add any interactivity or JavaScript functionality here
+// Example: console log when the page loads
+window.onload = function() {
+    console.log("Welcome to Cargills Pharmacy!");
+};
